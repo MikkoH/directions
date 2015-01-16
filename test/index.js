d0919abfd708e8eae0055f6f608ff78be8b698bb
Mikko Haapoja <me@mikkoh.com>
2015-01-15 23:38:44 -0500

    Added proper tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,20 @@
+var tape = require( 'tape' );
 var directions = require( './..' );
 
 var someDirections = directions();
 
-someDirections.fromTo( 'alpha', 'idle', 1 );
+someDirections.fromTo( 'alpha', 'idle', 1.2 );
 someDirections.fromTo( 'idle', 'rolled', 1 );
 someDirections.fromTo( 'rolled', 'idle', 1 );
-someDirections.fromTo( 'idle', 'omega', 1 );
+someDirections.fromTo( 'idle', 'omega', 1.5 );
 
-console.log( someDirections.getPath( 'alpha', 'omega' ) );
+tape( 'get path', function( t ) {
+
+  t.plan( 3 );
+
+  var data = someDirections.getPath( 'alpha', 'omega' );
+
+  t.equal( JSON.stringify( [ 'alpha', 'idle', 'omega' ] ), JSON.stringify( data.path ), 'path was correct' );
+  t.equal( 2.7, data.cost, 'path cost was correct' );
+  t.equal( 1.2, someDirections.getDuration( 'alpha', 'idle' ), 'individual duration was correct' );
+});
